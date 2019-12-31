4c0d7309071165e46fd8079db9e37049b9c9ffbe
sun <huachuang20@gmail.com>
2019-12-31 10:02:45 +0800

    add tdn_permission to dev

--- examples/simple.rs
+++ examples/simple.rs
@@ -1,31 +1,13 @@
 use async_std::task;
-use async_std::sync::channel;
-use tdn::{new_channel, start, Group, PeerId, GroupId};
-
-#[derive(Default)]
-struct SimpleGroup {
-    id: GroupId,
-    peers: Vec<PeerId>,
-}
-
-impl Group for SimpleGroup {
-    type JoinType = String;
-
-    fn id(&self) -> &GroupId {
-        &self.id
-    }
-
-    /// directly add a peer to group.
-    fn add(&mut self, peer_id: &PeerId) {
-        self.peers.push(peer_id.clone());
-    }
-}
-
+use tdn::{new_channel, start};
+use tdn_permission::PermissionlessGroup;
 
 fn main() {
     task::block_on(async {
         let (out_send, out_recv) = new_channel();
-        let send = start(out_send, SimpleGroup::default()).await.unwrap();
+        let send = start(out_send, PermissionlessGroup::default())
+            .await
+            .unwrap();
 
         while let Some(message) = out_recv.recv().await {
             println!("recv: {:?}", message);
